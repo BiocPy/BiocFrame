7a8350478960de573377a4ca765a6f0e4f618997
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-10 14:05:59 -0800

    Also removed the unnecessary types.

--- src/biocframe/types.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Optional, Tuple, Union
-
-__author__ = "jkanche"
-__copyright__ = "jkanche"
-__license__ = "MIT"
-
-SlicerTypes = Union[List[int], List[bool], List[str], slice, int, str]
-SlicerArgTypes = Union[List[str], Tuple[SlicerTypes, Optional[SlicerTypes]]]
